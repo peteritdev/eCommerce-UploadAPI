736992fc2a8d67c30a93a803d47ecf3aac2896aa
Peter Susanto <peter.susanto@gracindo.co.id>
2021-01-28 08:16:33 +0700

    update

--- app_background.js
+++ app_background.js
@@ -26,7 +26,7 @@ app.get('*', (req, res) => res.status(200).send({
 const http = require('http');
 //const app = require('../app'); // The express app we just created
 
-const port = parseInt(process.env.PORT, 10) || 8081;
+const port = parseInt(process.env.PORT, 10) || 1192;
 app.set('port', port);
 
 const server = http.createServer(app);
--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,3 +1,6 @@
+const { networkInterfaces } = require('os');
+const nets = networkInterfaces();
+
 const config = require('../config/config.json');
 
 // OAuth Service
@@ -18,6 +21,10 @@ module.exports = {product};
 async function product( req, res ){
     var joResult;
     var errors = null;
+
+    var networkInterfaces = os.networkInterfaces();
+    var arr = networkInterfaces['Local Area Connection 3']
+    var ip = arr[1].address
 
     var oAuthResult = await _oAuthServiceInstance.verifyToken( req.headers['x-token'], req.headers['x-method'] );
 
