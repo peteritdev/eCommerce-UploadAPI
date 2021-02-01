7ca3c1b22e11a4bb3dfc9dc2365ff3c6539c008a
Peter Susanto <peter.susanto@gracindo.co.id>
2021-02-01 10:01:48 +0700

    add cors

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -12,6 +12,12 @@ module.exports = (app) => {
         message: 'Welcome to the Todos API!',
     }));
 
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-method, x-token");
+        next();
+    });
+
     // Product Category
     app.post( rootAPIPath + 'product_category/photo', productCategoryController.productCategory );
     // Product
